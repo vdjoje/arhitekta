f3f5c60ddb110ce648b65e91d2671906eeab7f5b
Your Name <you@example.com>
2018-10-29 08:42:12 +0100

    home

--- src/components/Vila.js
+++ src/components/Vila.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { Component }from 'react'
 import { Card, Icon, Image } from 'semantic-ui-react'
+import Gallery from 'react-photo-gallery';
+import Lightbox from 'react-images';
 import s1 from '../images/Vila/1.jpg';
 import s2 from '../images/Vila/2.jpg';
 import s3 from '../images/Vila/3.jpg';
@@ -11,28 +13,70 @@ import s8 from '../images/Vila/8.jpg';
 import s9 from '../images/Vila/9.jpg';
 
 
+const photos = [
 
-const Vile = () => (
+    {src: s1, width: 6, height: 4 },
+    {src: s2, width: 6, height: 4 },
+    {src: s3, width: 6, height: 4 },
+    {src: s4, width: 6, height: 4 },
+    {src: s5, width: 6, height: 4 },
+    {src: s6, width: 6, height: 4 },
+    {src: s7, width: 6, height: 4 },
+    {src: s8, width: 6, height: 4 },
+    {src: s9, width: 6, height: 4 }
+];
+
+
+
+export default class Vile extends Component {
+    constructor() {
+      super();
+      this.state = { currentImage: 0 };
+      this.closeLightbox = this.closeLightbox.bind(this);
+      this.openLightbox = this.openLightbox.bind(this);
+      this.gotoNext = this.gotoNext.bind(this);
+      this.gotoPrevious = this.gotoPrevious.bind(this);
+    }
+    openLightbox(event, obj) {
+      this.setState({
+        currentImage: obj.index,
+        lightboxIsOpen: true,
+      });
+    }
+    closeLightbox() {
+      this.setState({
+        currentImage: 0,
+        lightboxIsOpen: false,
+      });
+    }
+    gotoPrevious() {
+      this.setState({
+        currentImage: this.state.currentImage - 1,
+      });
+    }
+    gotoNext() {
+      this.setState({
+        currentImage: this.state.currentImage + 1,
+      });
+    }
+    render() {
+      return (
+        <div
+            style={{
+                marginTop:'5%'    
+            }}
+        >  
+          <Gallery photos={photos} onClick={this.openLightbox} />
+          <Lightbox images={photos}
+            onClose={this.closeLightbox}
+            onClickPrev={this.gotoPrevious}
+            onClickNext={this.gotoNext}
+            currentImage={this.state.currentImage}
+            isOpen={this.state.lightboxIsOpen}
+            
+          />
+        </div>
+      )
+    }
+  }
   
-  <div
-      style={{color: "black", marginTop: "20%"}}
-  >
-      <h1>Enterijer vile na primorju</h1>
-      
-    <div>
-        <Image src={s1} />
-        <Image src={s2} />
-        <Image src={s3} />
-        <Image src={s4} />
-        <Image src={s5} />
-        <Image src={s6} />
-        <Image src={s7} />
-        <Image src={s8} />
-        <Image src={s9} />
-
-    </div>
-
-
-  </div>
-)
-export default Vile
